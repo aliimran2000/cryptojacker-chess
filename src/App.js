a9676c374a30ff9df1eefbfb35cc3d79f14fac86
aliimran2000 <muhammadaliimran2000@gmail.com>
2022-06-04 16:12:44 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+import { Chessboard } from "react-chessboard";
+import mySvg from "./wave.svg";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundImage: `url(${mySvg})`, height: "100%" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Chessboard id="BasicBoard" />
+      </div>
     </div>
   );
 }
-
-export default App;
